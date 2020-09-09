58845b43285173ea89ec6e9bd295b10dbcf74da8
BuildTools <ipagar@icloud.com>
2020-09-09 13:00:40 +0300

    Updates

---
+++ precache-manifest.fee682b92f8257004e5d6b272c43097d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8010904a32a3929e4900bd8518dd6ca",
+    "url": "/vcwebfix/index.html"
+  },
+  {
+    "revision": "06844dc371886953d36e",
+    "url": "/vcwebfix/static/css/main.a1107254.chunk.css"
+  },
+  {
+    "revision": "3653dceec2bb1395e4ce",
+    "url": "/vcwebfix/static/js/2.84911def.chunk.js"
+  },
+  {
+    "revision": "41b66b6fd6eecba581340794bb1831be",
+    "url": "/vcwebfix/static/js/2.84911def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06844dc371886953d36e",
+    "url": "/vcwebfix/static/js/main.eb903548.chunk.js"
+  },
+  {
+    "revision": "93dd72df658f831308a6",
+    "url": "/vcwebfix/static/js/runtime-main.1df80606.js"
+  },
+  {
+    "revision": "dfc54362fc2b0f7b99a4038b87dd9e64",
+    "url": "/vcwebfix/static/media/1.dfc54362.png"
+  },
+  {
+    "revision": "2048975925bd8553d8aaa5e1d9d1ffb1",
+    "url": "/vcwebfix/static/media/2.20489759.png"
+  },
+  {
+    "revision": "dbf82c38432d45de3c1c99f13c8b5fee",
+    "url": "/vcwebfix/static/media/3.dbf82c38.png"
+  },
+  {
+    "revision": "e671d53961f44185ee61256a0e6f13a5",
+    "url": "/vcwebfix/static/media/4.e671d539.png"
+  },
+  {
+    "revision": "35bcd06768ffafae4a5721b8f0c4fd8f",
+    "url": "/vcwebfix/static/media/burger.35bcd067.png"
+  },
+  {
+    "revision": "06277842ccc3f7c2a37085189dacdcbc",
+    "url": "/vcwebfix/static/media/check-symbol.06277842.svg"
+  },
+  {
+    "revision": "751f84c25a8d1ff159f499cbb9aa5bb7",
+    "url": "/vcwebfix/static/media/edit.751f84c2.svg"
+  },
+  {
+    "revision": "4fd2ba47b4f59d0a4382a8d81aae85d6",
+    "url": "/vcwebfix/static/media/kfc.4fd2ba47.png"
+  },
+  {
+    "revision": "60464810e8cfcde7a20fc19284c38a0e",
+    "url": "/vcwebfix/static/media/mcdac.60464810.png"
+  },
+  {
+    "revision": "1198ed41690cc0974aa8baa0e95ebd1b",
+    "url": "/vcwebfix/static/media/refresh-button.1198ed41.svg"
+  },
+  {
+    "revision": "4eb05fdd9f3d81f2f81cac5847e25cc9",
+    "url": "/vcwebfix/static/media/sub.4eb05fdd.png"
+  }
+]);
